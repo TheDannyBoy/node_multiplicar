75c6eb36ffcb2a20ece71fbe3d34157a421b07df
Daniel Vazquez <smashing_halo@hotmail.com>
2018-06-06 18:52:07 -0500

    Commit inicial

---
+++ archivos.js
@@ -0,0 +1,36 @@
+// const mult = require('./multiplicar/multiplicar');
+const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar');
+const { argv } = require('./config/yargs');
+const colors = require('colors');
+
+
+let comando = argv._[0];
+
+switch (comando) {
+    case 'listar':
+        listarTabla(argv.base, argv.limite);
+        break;
+    case 'crear':
+        crearArchivo(argv.base, argv.limite)
+            .then(archivo => console.log(`Archivo creado: ${ colors.green(archivo) }`))
+            .catch(err => console.log(err));
+        break;
+
+    default:
+        console.log('Comando no reconocido');
+        break;
+}
+
+
+console.log(argv);
+
+
+// console.log('Base', argv.base);
+
+
+
+// let argv2 = process.argv;
+// let base = argv2.split('=')[1]
+
+
+// console.log(`Base: ${ base }`);
